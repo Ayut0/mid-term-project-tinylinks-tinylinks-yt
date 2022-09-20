3c47ec19221011905e8490f322945db960befdd2
Yuto Yamakita <yuvto.0420@gmail.com>
2022-09-19 17:01:51 -0700

    Finished setting endpoints
    Only routing, not a function
    1. Edit
    2. Delete
    3. Create a new URL(register)
    
    Add two new keys to urls json
    1. Name of url to make users easy to recognize each url
    2. id of url to sort via id

--- controllers/urls.js
+++ controllers/urls.js
@@ -4,4 +4,50 @@ const showUrl = (req, res) => {
     res.render('urls', {urls: Object.values(urls)})
 }
 
-module.exports = { showUrl }
+const generateRandomUrl = () =>{
+    const result = Math.random().toString(36).substring(2,8);
+    console.log(result)
+    // return result
+}
+
+const createUrl = (req, res) =>{
+    generateRandomUrl();
+    res.render('newUrl');
+}
+
+//function to generate new shortened url
+const generateNewUrl = (req, res) =>{
+    console.log('registered nw url', req.body);
+    res.redirect('/urls')
+}
+
+//Single
+const singleUrl = (req, res) =>{
+    const urlId = Number(req.params.id);
+    const urlList = Object.values(urls);
+    const url = urlList.find((url) => url.id === urlId);
+
+    url ?
+    (res.render('singleUrl.ejs', {url: url}))
+    :
+    res.send('seems like you have no url matches the id');
+}
+
+const editUrl = (req, res) =>{
+    console.log('Edit url', req.body)
+    res.send('Edited')
+}
+
+//Make sure if the shortened url exists
+const redirectToRealUrl = (req, res) =>{
+    const urlId = Number(req.params.id);
+    const urlList = Object.values(urls);
+    const url = urlList.find((url) => url.id === urlId);
+
+    url?
+    (res.redirect(url.longUrl))
+    :
+    (res.send('Hmm... we failed to find the URL'));
+}
+
+module.exports = { showUrl, createUrl, generateNewUrl, singleUrl, editUrl, redirectToRealUrl, generateRandomUrl }
--- routes/urls.js
+++ routes/urls.js
@@ -1,12 +1,34 @@
 const express =  require('express');
 const router = express.Router();
-const { showUrl } = require('../controllers/urls.js');
+const { showUrl, createUrl, generateNewUrl, singleUrl, editUrl, redirectToRealUrl } = require('../controllers/urls.js');
+const  urls = require('../models/urls.json')
 const { showLogin, showRegister } = require('../controllers/auth.js');
 
 router.get('/', showUrl);
 
-//router.get('/:id', ); add function to receive the id
-//router.post('/:id', ); add function to receive the id - edit
-//router.post('/:id', ); add function to receive the id - delete
+//Create
+router.get('/new', createUrl)
+router.post('/new', generateNewUrl)
 
+//edit
+//router.post('/:id', ); add function to receive the id - edit
+router.get('/:id/edit', (req, res, next) =>{
+    const urlId = req.params.id;
+    res.json({message: 'edit url'})
+})
+//delete
+//router.post('/:id', ); add function to receive the id - delete
+router.post('/:id/delete', (req, res, next) =>{
+    const urlId = req.params.id;
+
+    res.json({message: 'Delete url'})
+})
+
+//Single url
+// router.get('/:id', ); add function to receive the id
+router.get('/:id', singleUrl)
+router.post('/:id', editUrl)
+
+//Make sure if the shortened url exists
+router.get('/u/:id', redirectToRealUrl)
 module.exports = router;
